37f069150dce2416dc3a8da3d5668e04f0acf049
MarceloDataLab <angelmarcelorodriguez1999@gmail.com>
2024-08-15 11:24:16 +0100

    update System.py 3

--- System.py
+++ System.py
@@ -8,9 +8,6 @@ class Dealer:
     def __init__(self):
         self.hand = []
     
-    def play_hand(self):
-        pass    
-
 class Player:
     """
     The Player class represents a player in the Blackjack game. 
@@ -25,10 +22,8 @@ class Player:
     def make_deposit(self,deposit):
         self.balance += deposit
         self.deposit_history.append(deposit)
-        
-    def play_cards(self):
-        pass
     
+
     def __repr__(self):
         return f"Your balance is {self.balance}$ and your earnings are {self.balance - sum(self.deposit_history)}$"
     
@@ -71,14 +66,102 @@ class BlackJack:
             self.player.make_deposit(deposit)
             print("deposited successfully!")
     
+    def calculate_hand_value(self,cards):
+        
+        blackjack_values = {'0': 0, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 11}
+
+        count = 0
+        num_aces = 0 
+        for card in cards:
+            count += blackjack_values.get(card[0])
+            if blackjack_values.get(card[0]) == "A":
+                num_aces += 1
+        if num_aces > 0 and count > 21:
+            count -= num_aces * 10
+        
+        return count
+         
+    def player_play(self):
+        value = self.calculate_hand_value(self.player.hand)
+        print(f"{self.player.hand} is {value}")
+        
+        if len(self.player.hand) < 3 and value == 21:
+            return 1000
+            
+        elif self.player.hand[0][0] == self.player.hand[1][0]:
+            print("Play Double")
+            deck1 = [self.player.hand[0]]
+            deck1.append(self.cards.one_card())
+            print(f"First hand: {deck1}")
+            value = self.calculate_hand_value(deck1)
+            if len(deck1) < 3 and value == 21:
+                return 1000
+            
+            while True:
+                dec = input("Hit or Stay: ")
+                if dec == "stay":
+                    break
+                elif dec == "hit":    
+                    deck1.append(self.cards.one_card())
+                    value = self.calculate_hand_value(deck1)
+                    print(f"{deck1} is {value}")
+                    if value > 21:
+                        deck1 = [["0", "0"]]
+                        break
+                    elif value == 21:
+                        return 21
+                                  
+            deck2 = [self.player.hand[1]]
+            deck2.append(self.cards.one_card())
+            print(f"second hand: {deck2}")
+            
+            while True:
+                dec = input("Hit or Stay: ")
+                if dec == "stay":
+                    break
+                elif dec == "hit":    
+                    deck2.append(self.cards.one_card())
+                    value = self.calculate_hand_value(deck2)
+                    print(f"{deck2} is {value}")
+                    if value > 21:
+                        deck2 = [["0", "0"]]
+                        break
+                    elif value == 21:
+                        return 21
+                                  
+            result_deck1, result_deck2 = self.calculate_hand_value(deck1), self.calculate_hand_value(deck2)
+            return max(result_deck1, result_deck2)
+        
+        else:
+            dec = input("Hit or Stay: ")
+            if dec == "stay":
+                return self.calculate_hand_value(self.player.hand)
+            while dec != "stay":
+                self.player.hand.append(self.cards.one_card())
+                value = self.calculate_hand_value(self.player.hand)
+                print(f"{self.player.hand} is {value}")
+                if value > 21:
+                    return 0
+                elif value == 21:
+                    return 21
+                dec = input("Hit or Stay: ")
+            return self.calculate_hand_value(self.player.hand)                
+                               
+    def dealer_play(self):
+        return 19
+    
+    def check_result(self, result_player, result_dealer):
+        pass
+    
     def start(self):
         self.cards.create_deck()
         self.player.hand.append(self.cards.one_card())
         self.player.hand.append(self.cards.one_card())
         self.dealer.hand.append(self.cards.one_card())
         self.dealer.hand.append(self.cards.one_card())
-
-
+        result_player = self.player_play()
+        result_dealer = self.dealer_play()
+        
     def open(self,play_again=True):
         return play_again
       
